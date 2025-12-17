657e2e09c6aea4537e69c718a6079163b6a4130d
Ken Keller <ken.keller@hashicorp.com>
2025-12-16 18:21:57 -0600

    add output.tf to app folder

--- terraform/app/output.tf
+++ terraform/app/output.tf
@@ -1,10 +1,7 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 output "external_vault_addr" {
    description = "External Vault address for Vault Agent to connect to"
    value       = "http://${data.external.get-k8s-host.result["EXTERNAL_VAULT_ADDR"]}:8200"
-} 
-
-
-output "raw" {
-   description = "External Vault address for Vault Agent to connect to"
-   value       = data.external.get-k8s-host.result
 } 
